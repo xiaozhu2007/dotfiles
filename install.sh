58cf98ae436072b293e720d586240ba1331ee0ad
HackPig520 / 甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2025-04-02 13:51:28 +0800

    Update install.sh
    Signed-off-by: HackPig520 / 甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -4,8 +4,18 @@
 # Thanks https://github.com/CoreyMSchafer/dotfiles/blob/master/install.sh
 ############################
 
+set -e
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Error: This script must be run as root!" 1>&2
+    exit 1
+fi
+
 # dotfiles directory
 dotfiledir="${HOME}/dotfiles"
+
+# Remove existing dotfiles directory
+rm -r "${dotfiledir}"
 
 # list of files/folders to symlink in ${homedir}
 files=(bashrc bash_profile bash_prompt aliases func profile)
@@ -18,10 +28,16 @@ git clone https://github.com/xiaozhu2007/dotfiles ~/dotfiles
 echo "Changing to the ${dotfiledir} directory"
 cd "${dotfiledir}" || exit
 
-# create symlinks (will overwrite old dotfiles)
+# Create symlinks (will overwrite existing files)
 for file in "${files[@]}"; do
+    src="${dotfiledir}/${file}"
+    dest="${HOME}/.${file}"
+    if [ ! -e "$src" ]; then
+        echo "Error: Source file $src does not exist."
+        exit 1
+    fi
     echo "Creating symlink to $file in home directory."
-    ln -sf "${dotfiledir}/.${file}" "${HOME}/.${file}"
+    ln -sf "$src" "$dest"
 done
 
 # install tools
@@ -42,12 +58,15 @@ rm fzf-0.61.0-linux_amd64.tar.gz
 # fd
 wget https://github.com/sharkdp/fd/releases/download/v10.2.0/fd_10.2.0_amd64.deb
 apt install -y ./fd_10.2.0_amd64.deb
+rm fd_10.2.0_amd64.deb
 # bat
 wget https://github.com/sharkdp/bat/releases/download/v0.25.0/bat_0.25.0_amd64.deb
 apt install -y ./bat_0.25.0_amd64.deb
+rm bat_0.25.0_amd64.deb
 
 # chmod & move
 chmod +x fzf rg
 mv fzf /usr/bin
 mv rg /usr/bin
 echo "Installation Complete!"
+echo "Want to update? cd /dotfiles && gp "
