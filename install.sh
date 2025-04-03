0c0666b29cbc1fa353765e13a719ef7b010fccaf
HackPig520 / 甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2025-04-03 17:10:58 +0800

    Update install.sh
    Signed-off-by: HackPig520 / 甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -27,15 +27,21 @@ rm -r "${dotfiledir}"
 # list of files/folders to symlink in ${homedir}
 files=(bashrc bash_profile bash_prompt aliases func profile)
 
+# =================
 # clone the repo
+# =================
 apt install -y git
 git clone https://github.com/xiaozhu2007/dotfiles ~/dotfiles
 
+# =================
 # change to the dotfiles directory
+# =================
 echo "Changing to the ${dotfiledir} directory"
 cd "${dotfiledir}" || exit
 
+# =================
 # Create symlinks (will overwrite existing files)
+# =================
 for file in "${files[@]}"; do
     src="${dotfiledir}/.${file}"
     dest="${HOME}/.${file}"
@@ -47,9 +53,11 @@ for file in "${files[@]}"; do
     ln -sf "$src" "$dest"
 done
 
+# =================
 # install tools
+# =================
 apt update
-apt install -y curl wget tar unzip zip
+apt install -y curl wget tar unzip zip tealdeer exa
 # ripgrep
 wget https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-x86_64-unknown-linux-musl.tar.gz
 tar xvf ripgrep-14.1.1-x86_64-unknown-linux-musl.tar.gz
@@ -59,9 +67,6 @@ rm -r ripgrep-14.1.1-x86_64-unknown-linux-musl/ ripgrep-14.1.1-x86_64-unknown-li
 wget https://github.com/junegunn/fzf/releases/download/v0.61.0/fzf-0.61.0-linux_amd64.tar.gz
 tar xvf fzf-0.61.0-linux_amd64.tar.gz
 rm fzf-0.61.0-linux_amd64.tar.gz
-# onefetch
-# wget https://github.com/o2sh/onefetch/releases/download/2.23.1/onefetch_2.23.1_amd64.deb
-# apt install -y ./onefetch_2.23.1_amd64.deb
 # fd
 wget https://github.com/sharkdp/fd/releases/download/v10.2.0/fd_10.2.0_amd64.deb
 apt install -y ./fd_10.2.0_amd64.deb
@@ -71,8 +76,17 @@ wget https://github.com/sharkdp/bat/releases/download/v0.25.0/bat_0.25.0_amd64.d
 apt install -y ./bat_0.25.0_amd64.deb
 rm bat_0.25.0_amd64.deb
 
+# =================
 # chmod & move
+# =================
 chmod +x fzf rg
 mv fzf /usr/bin
 mv rg /usr/bin
-echo "Installation Complete!"
+
+echo ""
+echo "+-------------------------------+"
+echo "|     Installation Complete     |"
+echo "| Installed: tldr, fd, bat, rg, |"
+echo "|           fzf, exa            |"
+echo "+-------------------------------+"
+echo ""
