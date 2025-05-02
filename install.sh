bb440df3e92f9fbc2c297a84282b3f66890e6007
liuzhen932 <im@932.moe>
2025-05-02 20:29:50 +0800

    add hexyl
    https://github.com/sharkdp/hexyl

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ done
 # install tools
 # =================
 apt update
-apt install -y curl wget tar unzip zip tealdeer
+apt install -y curl wget tar unzip zip tealdeer hexyl
 # ripgrep
 wget https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-x86_64-unknown-linux-musl.tar.gz
 tar xvf ripgrep-14.1.1-x86_64-unknown-linux-musl.tar.gz
